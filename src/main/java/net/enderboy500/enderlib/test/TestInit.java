ff29fee67a0f2f22a7cac8bb92671aa64f2c3af1
Enderboy <136089753+EnderBoy500@users.noreply.github.com>
2025-09-26 14:51:21 +0300

    forgot something

--- src/main/java/net/enderboy500/enderlib/test/TestInit.java
+++ src/main/java/net/enderboy500/enderlib/test/TestInit.java
@@ -6,6 +6,6 @@ import net.minecraft.entity.effect.StatusEffectCategory;
 import net.minecraft.item.Item;
 
 public class TestInit {
-    public static final Item TEST = RegistryHelper.registerItem("test", TestItem::new, new Item.Settings());
+    //public static final Item TEST = RegistryHelper.registerItem("test", TestItem::new, new Item.Settings());
     public static void init() {}
 }
